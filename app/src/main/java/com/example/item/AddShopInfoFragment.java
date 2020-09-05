ff9fbfae9cbcb2fa419e58d063232fa889b9bdaf
abineko2 <yuki1980426@gmail.com>
2020-09-06 02:01:11 +0900

    test

--- app/src/main/java/com/example/item/AddShopInfoFragment.java
+++ app/src/main/java/com/example/item/AddShopInfoFragment.java
@@ -6,13 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
 public class AddShopInfoFragment extends Fragment{
-
+     private EditText shopInput, emailInput, passwordInput, passwordConfirmationInput, telInput;
+     private TextView shopError, emailError, passwordError, confirmationError;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
@@ -22,6 +25,65 @@ public class AddShopInfoFragment extends Fragment{
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        /*editText一覧*/
+
+        shopInput = view.findViewById(R.id.shop_name_edit);
+        emailInput = view.findViewById(R.id.add_shop_mail);
+        passwordInput = view.findViewById(R.id.add_shop_pass);
+        passwordConfirmationInput = view.findViewById(R.id.add_shop_pass_confirmation);
+        telInput = view.findViewById(R.id.add_shop_tel);
+
+        /*警告テキスト*/
+        shopError = view.findViewById(R.id.shop_name_validation);
+        emailError = view.findViewById(R.id.shop_mail_validation);
+        passwordError = view.findViewById(R.id.shop_pass_validation);
+        confirmationError = view.findViewById(R.id.shop_conf_validation);
+
+        /* 登録作業*/
+        Button shopAdd = view.findViewById(R.id.add_shop_button);
+        shopAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String shop = shopInput.getText().toString();                                     /*店舗書き込み内容*/
+                String email = emailInput.getText().toString();                                   /*email書き込み内容*/
+                String password = passwordInput.getText().toString();                            /*password書き込み内容*/
+                String passwordConfirmation = passwordConfirmationInput.getText().toString();    /*passwordConfirmation書き込み内容*/
+
+                /*各種ERROR対応または通信*/
+
+                if(shop.equals("")){ shopError.setVisibility(View.VISIBLE); }
+                else{shopError.setVisibility(View.GONE);}
+
+
+                if(email.equals("")){ emailError.setVisibility(View.VISIBLE); }
+                else{ emailError.setVisibility(View.GONE);}
+
+
+
+                if(password.equals("")){
+                    passwordError.setText("(必須)パスワードを入力ください。");
+                    passwordError.setVisibility(View.VISIBLE);
+                }
+                else{ passwordError.setVisibility(View.GONE);}
+
+                if(passwordConfirmation.equals("")){
+                    confirmationError.setText("(必須)パスワードを入力ください。");
+                    confirmationError.setVisibility(View.VISIBLE);
+                }
+                else { confirmationError.setVisibility(View.GONE);}
+
+                if(!password.equals("") && !password.equals("")){
+                    passwordError.setVisibility(View.GONE);
+                    confirmationError.setVisibility(View.GONE);
+                }
+
+
+            }
+        });
+
+
+
 
         /*店舗一覧に戻る*/
         Button back = view.findViewById(R.id.back_add_shop_button);
---
+++ app/src/main/java/com/example/item/AddShopTask.java
@@ -0,0 +1,82 @@
+package com.example.item;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class AddShopTask extends AsyncTask<String, Void, StringBuilder> {
+
+
+    // 非同期処理
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected StringBuilder doInBackground(String... params) {
+
+        // 使用するサーバーのURLに合わせる
+        String urlSt = "http://yukiabineko.sakura.ne.jp/items/shopAdd.php";
+
+        HttpURLConnection httpConn;
+
+        String word = "user_id=" + params[0] + "&item_id=" + params[1] + "&confirm=" + params[2];
+        System.out.println(word);
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            // URL設定
+            URL url = new URL(urlSt);
+
+            // HttpURLConnection
+            httpConn = (HttpURLConnection) url.openConnection();
+
+            // request POST
+            httpConn.setRequestMethod("POST");
+
+            // no Redirects
+            httpConn.setInstanceFollowRedirects(false);
+
+            // データを書き込む
+            httpConn.setDoOutput(true);
+
+            // 時間制限
+            httpConn.setReadTimeout(6000);
+            httpConn.setConnectTimeout(5000);
+
+            // 接続
+            httpConn.connect();
+
+            try (// POSTデータ送信処理
+                 OutputStream outStream = httpConn.getOutputStream()) {
+                outStream.write(word.getBytes(StandardCharsets.UTF_8));
+                outStream.flush();
+                Log.d("debug", "flush");
+
+                // データを受け取る
+                InputStream is = httpConn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                String line;
+                while ((line = reader.readLine()) != null)
+                    sb.append(line);
+                is.close();
+            } catch (IOException e) {}
+        }
+        catch (IOException e){}
+        return sb;
+    }
+    // 非同期処理が終了後、結果をメインスレッドに返す
+    public void onPostExecute(StringBuilder result){
+
+
+    }
+
+}
